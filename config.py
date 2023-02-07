a4d7305d1a7271b78e1b46314e36123f2e9b5b06
Nyxordinal <nyxordinal@gmail.com>
2023-02-08 00:00:33 +0700

    change from using config file to env file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-import configparser
 import json
+import os
 
 from constant import CLIENT_SECRETS_FILE
 from util import write_to_file
@@ -7,13 +7,11 @@ from util import write_to_file
 
 class Config:
     def __init__(self) -> None:
-        config = configparser.RawConfigParser()
-        config.read(r'./config.txt')
-        self.redis_host = config.get('app', 'redis_host')
-        self.redis_port = config.get('app', 'redis_port')
-        self.discord_token = config.get('app', 'discord_token')
-        self.bot_channel_name = config.get('app', 'bot_channel_name')
+        self.redis_host = os.getenv("REDIS_HOST", "localhost")
+        self.redis_port = os.getenv("REDIS_PORT", "6379")
+        self.discord_token = os.getenv("DISCORD_TOKEN", "")
+        self.bot_channel_name = os.getenv("BOT_CHANNEL_NAME", "")
 
-        client_secret_raw = config.get('app', 'client_secrets_json')
+        client_secret_raw = os.getenv("CLIENT_SECRETS_JSON", "")
         client_secret_json = json.loads(client_secret_raw)
         write_to_file(CLIENT_SECRETS_FILE, client_secret_json)
--- main.py
+++ main.py
@@ -1,34 +1,31 @@
 import logging as log
 import os
 
+from dotenv import load_dotenv
+
 from config import Config
 from constant import CLIENT_SECRETS_FILE
 from dc import DiscordClient
 from rd import Redis
 from yt import YoutubeClient
 
-os.path.abspath(os.path.join(os.path.dirname(
-    __file__), CLIENT_SECRETS_FILE))
+load_dotenv()
+os.path.abspath(os.path.join(os.path.dirname(__file__), CLIENT_SECRETS_FILE))
 
 if __name__ == "__main__":
     log.basicConfig(
-        level=log.INFO,
+        level=log.INFO if os.getenv("ENV", "development") == "production" else log.DEBUG,
         format='%(asctime)s|%(name)s|%(levelname)s|%(message)s',
         datefmt='%d-%b-%y %H:%M:%S'
     )
     logger = log.getLogger()
-
     logger.info("starting bot...")
-
     logger.info("loading config...")
     cf = Config()
-
     logger.info("setup redis")
     rd = Redis(logger=logger, config=cf)
-
     logger.info("setup youtube client")
     yt_inst = YoutubeClient()
-
     logger.info("setup discord client")
     discord = DiscordClient(
         logger=logger,
